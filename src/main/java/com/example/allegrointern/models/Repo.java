91848cbc650f44f93754c1b714ad26c556fd8863
FilipKudlacz <kudlacz1998@gmail.com>
2019-04-19 07:35:07 +0200

    Deserialization from Json to object Repo

--- src/main/java/com/example/allegrointern/models/Repo.java
+++ src/main/java/com/example/allegrointern/models/Repo.java
@@ -1,7 +1,38 @@
 package com.example.allegrointern.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "name",
+        "updated_at"
+})
 public class Repo {
 
-    public String name, updated;
+    private String name;
 
+    @JsonProperty("updated_at")
+    private String updated;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("updated_at")
+    public String getUpdated() {
+        return updated;
+    }
+
+    @JsonProperty("updated_at")
+    public void setUpdated(String updated) {
+        this.updated = updated;
+    }
 }
